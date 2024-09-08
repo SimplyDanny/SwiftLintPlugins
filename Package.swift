7c80ce6f142164b0201871e580b021d1b2c69804
Danny Mösch <danny.moesch@icloud.com>
2024-09-08 19:12:05 +0200

    Fix version

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SwiftLintBinary",
-                url: "https://github.com/realm/SwiftLint/releases/download/0.56.2/SwiftLintBinary-macos.artifactbundle.zip",
+                url: "https://github.com/realm/SwiftLint/releases/download/0.57.0/SwiftLintBinary-macos.artifactbundle.zip",
                 checksum: "a1bbafe57538077f3abe4cfb004b0464dcd87e8c23611a2153c675574b858b3a"
         )
     ]
