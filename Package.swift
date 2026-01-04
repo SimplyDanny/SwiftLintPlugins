707f7face5524fe918f82dd39c5c80cb3e591a44
Danny Mösch <danny.moesch@icloud.com>
2026-01-04 11:05:58 +0100

    Fix URL for 0.63.0 release

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.63.0-rc.2/SwiftLintBinary.artifactbundle.zip",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.63.0/SwiftLintBinary.artifactbundle.zip",
             checksum: "b51ca39ffe2331fe0337f9267d4b0dea2c182791a2fd0f3b961d7cbfb6d488d7"
         )
     ]
